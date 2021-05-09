97d9568dceaee723fa6b4a645c711b9b25c9e16e
Dev Parikh <47342287+devparikh@users.noreply.github.com>
2021-05-09 17:12:14 -0400

    Update YOLO.py

--- YOLO.py
+++ YOLO.py
@@ -11,56 +11,51 @@ with open("C:\\Users\\me\\Documents\\Python\\YOLO\\coco.names", "r") as r:
   # here we are reading the image in and doing splitline which splits a string into a list
   classes = r.read().splitlines()
 # this should be 80
-len(classes)
+print(len(classes))
+
+
+print("[INFO] loading YOLO from disk...")
+net = cv2.dnn.readNetFromDarknet("C:\\Users\\me\\Documents\\Python\\YOLO\\yolov3-tiny.cfg", "C:\\Users\\me\\Documents\\Python\\YOLO\\yolov3-tiny.weights")
+
 
 image = cv2.imread("image.jpeg")
 image_size = 256
 blob = cv2.dnn.blobFromImage(image, 1 / 255.0, size=(256, 256), swapRB=True)
 
 
-# getting the input as this new_image
-yolo_network.setInput(blob)
-# returns the indexes of layers with unconnected outputs
-ln = yolo_network.getLayerNames()
-output_layer = yolo_network.getUnconnectedOutLayersNames()
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
-layersoutput = yolo_network.forward(output_layer)
+blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (256, 256),
+	swapRB=True, crop=False)
+net.setInput(blob)
+layerOutputs = net.forward(ln)
 
-bounding_boxes = []
-confidence = []
-classid = []
+boxes = []
+confidences = []
+classIDs = []
+thres = 0.5
 
+for output in layerOutputs:
+	for detection in output:
+		scores = detection[5:]
+		classID = np.argmax(scores)
+		confidence = scores[classID]
 
-# now we need to retrieve all of the data from the model
-for output in layersoutput:
-   for detection in output:
-      # every thing after the 4th index in the vector returned by the model can be used for the scores
-      # because the previous 4 indexes are height, width and the x and y coordinates of the center point of each grid in the image
-      # after the specifications of the bounding box the next thing is to get the class and the confidence score 
-      # everthing after the 4th index will be a class as there are 80 classes in the dataset
-      scores = detection[5:]
-      # we want to find the highest value in the distribution probablity of the classes and that will be our class
-      object_class = np.argmax(scores)
-      # now you want wha the confidence score is of the object in the grid
-      # the best thing to do is to find the probablity that the object class has and save that as the confidence
-      confidence_score = scores[object_class]
-      # this is the threshold that we have put to filter out any weak detections
-      thres = 0.6
-      # we will now create the 
-      if confidence_score > thres:
-        bounding_box = detection[0:4] * np.asarray([image_size,image_size,image_size,image_size])
-        # the astype method converts any value in any form to whatever form specified
-        (x_center, y_center, image_size, image_size) = bounding_box.astype("int")
+		if confidence > thres:
+			box = detection[0:4] * np.array([image_size, image_size, image_size, image_size])
+			(centerX, centerY, width, height) = box.astype("int")
 
-        x = int(x_center - (0.5*image_size))
-        y = int(y_center - (0.5*image_size))
+			x = int(centerX - (width / 2))
+			y = int(centerY - (height / 2))
 
-        classid.append(int(object_class))
-        confidence.append(int(confidence_score))
-        bounding_boxes.append(bounding_box)      
+			boxes.append([x, y, int(width), int(height)])
+			confidences.append(float(confidence))
+			classIDs.append(classID)
+print(scores)
 # non-max-supression
-nms_threshold = 0.4
-nms = cv2.dnn.NMSBoxes(bounding_boxes, confidence, thres, nms_threshold)
+nms_threshold = 0.7
+nms = cv2.dnn.NMSBoxes(boxes, confidences, thres, nms_threshold)
 
 COLOURS = []
 while len(COLOURS) < 80:
@@ -73,17 +68,17 @@ while len(COLOURS) < 80:
   # appending this colour to the list to it can be used later
   COLOURS.append(colour)
 
-# here we are checking if there is any bounding boxes after non-maxima supression
 if len(nms) > 0:
-  # we collapse the array into a 1-dimensional vector to iterate over
-  for i in nms.flatten():
-    # we want to choice a random colour from the COLOURS list
-    colours = random.choice(COLOURS)
-    # drawing the bounding box
-    cv2.rectangle(image, (x,y), (x+image_size, y+image_size), colours, 2)
-    # labelling the bounding box
-    cv2.putText(image, "class", classid, "confidence", confidence, (x,y), cv2.FONT_HERSHEY_COMPLEX, colours, 2)
-# displaying the bounding box
+	for i in nms.flatten():
+		(x, y) = (boxes[i][0], boxes[i][1])
+		(w, h) = (boxes[i][2], boxes[i][3])
+
+		color = [int(c) for c in COLOURS[classIDs[i]]]
+		cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+		text = "{}: {:.4f}".format(classes[classIDs[i]], confidences[i])
+		cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
+			0.5, color, 2)
+
 cv2.imshow("Image", image)
-# saving the image
-cv2.imwrite("image2.jpeg", image)
+cv2.imwrite("image.jpeg", image)
+cv2.waitKey(0)
